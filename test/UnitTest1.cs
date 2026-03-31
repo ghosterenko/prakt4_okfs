a66223227a34d610b6f44a378c8f3da5b622dbe7
ghosterenko <arsenijkonkov243@gmail.com>
2026-03-31 14:30:45 +0500

    res

--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -59,27 +59,6 @@ namespace test
             Assert.Equal("error", ex.Message);
 
         }
-        [Fact]
-        public void CheckMethodForTriangleArea ()
-        {
-            Triangle triangle = new Triangle( );
-            triangle.SideA = 13;
-            triangle.SideB = 14;
-            triangle.SideC = 15;
-
-            var res = 84;
-            Assert.Equal(res, triangle.Area( ));
-        }
-        [Fact]
-        public void CheckMethodForTrianglePerimetr ()
-        {
-            Triangle triangle = new Triangle( );
-            triangle.SideA = 10;
-            triangle.SideB = 10;
-            triangle.SideC = 10;
-
-            var res = 30;
-            Assert.Equal(res, triangle.Perimeter( ));
-        }
+       
     }
 }
